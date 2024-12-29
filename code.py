9eb8014d2d4d6201950b3d5fd951085aa2333ba8
Nykio <164542486+Nykio@users.noreply.github.com>
2024-12-29 12:33:22 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,34 @@
-class Attaques :
-	def __init__(self,Nom,Degats):
-		self.nom = Nom
-		self.dgts = Degats
+faiblesse = {"Nature" : "Mana" , 
+            "Mana" : "Ombre" , 
+            "Ombre" : "Nature"}
+
+def dedans(element , conteneur):
+    for a in conteneur :
+        if a == element :
+            return True
+            break
+    return False
+
+from random import randint 
+
+class Attaques:
+    def __init__(self, Nom, Degats, Element):
+        self.nom = Nom
+        self.dgts = Degats
+        self.ele = Element
+
+    def utiliser(self) :
+        aleatoire = randint(1,5) #va nous permettre de faire des coups critiques
+        if aleatoire == 1 :
+            return self.dgts*1.15
+        else :
+            return self.dgts
 
 class Potion :
     def __init__(self,Soin):
         self.soin = Soin
       
+
 class Cartes :
     def __init__(self , PV , Vitesse):
         self.pv = PV
@@ -25,6 +47,20 @@ class Cartes :
         if self.pv > self.pvmax :
             self.pv = self.pvmax
 
+    def choix_attaque(self) :
+        possibilité = [] #va nous permettre de vérifier que la demande du joueur est possible
+        print("Entrez le numéro de l'attaque que vous voulez utiliser : ")
+        for i , attaque in enumerate(self.atq) :
+            possibilité.append(i)
+            print(f"{i} - {attaque.nom}")
+        entree = int(input())
+        for i , attaque in enumerate(self.atq) :
+            if i == entree :
+                return attaque
+            else :
+                print("Choix non reconnu veuillez recommencer .")
+        return self.choix_attaque()
+
 class Joueur:
     def __init__(self, deck, sac):
         self.deck = []
@@ -38,3 +74,18 @@ class Joueur:
 
     def sors_carte(self, carte):
         self.deck.remove(carte)
+
+    def choisir_carte(self) :
+        pass
+
+    def choisir_carte_aleatoire(self) :
+        pass
+
+class Combat :
+    def __init__(self , joueur:Joueur , joueur_ennemi:Joueur):
+        pass
+
+    def lancer() :
+        Carte1 = joueur.choisir_carte()
+        Carte2 = jouer_ennemi.choisir_carte_aleatoire()
+

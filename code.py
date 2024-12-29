706dc73173009870b274f059d30bb97b8744bc51
Nykio <164542486+Nykio@users.noreply.github.com>
2024-12-29 16:27:45 +0100

    Update code.py

--- code.py
+++ code.py
@@ -28,7 +28,6 @@ class Potion :
     def __init__(self,Soin):
         self.soin = Soin
       
-
 class Cartes :
     def __init__(self , PV , Vitesse):
         self.pv = PV
@@ -39,9 +38,9 @@ class Cartes :
     def apprendre_attaque(self,attaque):
         self.atq.append(attaque)
         
-    def soigner(self,potion):
+    def soigner(self,potion:Potion):
         if self.pv == self.pvmax :
-            print("la carte a déjà tous ses points de vies")
+            print("la carte a déjà tous ses points de vies \n")
         else:
             self.pv += potion.soin
         if self.pv > self.pvmax :
@@ -49,10 +48,13 @@ class Cartes :
 
     def est_en_vie(self):
         pass 
-    
+
+    def choix_auto(self) :
+        pass
+
     def choix_attaque(self) :
         possibilité = [] #va nous permettre de vérifier que la demande du joueur est possible
-        print("Entrez le numéro de l'attaque que vous voulez utiliser : ")
+        print("Entrez le numéro de l'attaque que vous voulez utiliser :  \n")
         for i , attaque in enumerate(self.atq) :
             possibilité.append(i)
             print(f"{i} - {attaque.nom}")
@@ -61,11 +63,12 @@ class Cartes :
             if i == entree :
                 return attaque
             else :
-                print("Choix non reconnu veuillez recommencer .")
+                print("Choix non reconnu veuillez recommencer . \n")
         return self.choix_attaque()
 
 class Joueur:
-    def __init__(self, deck, sac):
+    def __init__(self,nom , deck, sac):
+        self.nom = nom
         self.deck = []
         self.sac = []
         
@@ -84,14 +87,62 @@ class Joueur:
     def choisir_carte_aleatoire(self) :
         pass
 
-class Combat :
-    def __init__(self , joueur:Joueur , joueur_ennemi:Joueur):
+class Combat:
+    def __init__(self, joueur: Joueur, joueur_ennemi: Joueur):
+        self.joueur = joueur
+        self.joueur_ennemi = joueur_ennemi
 
-    def round() :
-        Carte1 = joueur.choisir_carte()
-        Carte2 = joueur_ennemi.choisir_carte_aleatoire()
-        if Carte1.vts > Carte2.vts : #va nous permettre de savoir quelle carte a jouer precedement
-            Carte1_round_precedent = False
-        else :
-            Carte1_round_precedent = True
-        while Carte1.pv
+    def joueur_attaque(self, attaquant: Cartes, cible: Cartes):
+        attaque = attaquant.choix_attaque()
+        degats = attaque.utiliser()
+
+        if faiblesse[attaque.ele] == cible.ele:
+            degats *= 2
+            print(f"\nCoup critique ! {attaque.nom} inflige {degats} dégâts à {cible.nom}.")
+
+        elif faiblesse[cible.ele] == attaque.ele:
+            degats *= 0.5
+            print(f"\nCoup peu efficace... {attaque.nom} inflige {degats} dégâts à {cible.nom}.")
+
+        else:
+            print(f"\n{attaque.nom} inflige {degats} dégâts à {cible.nom}.")
+
+        cible.pv -= degats
+
+    def round(self):
+        Carte1 = self.joueur.choisir_carte()
+        Carte2 = self.joueur_ennemi.choisir_carte_aleatoire()
+
+        print(f"\n{Carte1.nom} - {Carte1.pv} affronte {Carte2.nom} - {Carte2.pv} !")
+
+        tour_joueur = False
+        if Carte1.vts > Carte2.vts :
+            tour_joueur = True
+
+        while Carte1.est_en_vie() and Carte2.est_en_vie():
+            if tour_joueur :
+                self.joueur_attaque(Carte1, Carte2)
+                tour_joueur = False
+            else:
+                self.joueur_attaque(Carte2, Carte1)
+                tour_joueur = True
+
+            print(f"\n{Carte1.nom} a {Carte1.pv} PV , {Carte2.nom} a {Carte2.pv} PV")
+
+
+        if Carte1.est_en_vie():
+            print(f"\n{Carte2.nom} est hors jeu !")
+            self.joueur_ennemi.sors_carte(Carte2)
+        else:
+            print(f"\n{Carte1.nom} est hors jeu !")
+            self.joueur.sors_carte(Carte1)
+
+    def lancer(self):
+        print(f"\nLe combat commence entre {self.joueur.nom} et {self.joueur_ennemi.nom} !")
+        while self.joueur.deck and self.joueur_ennemi.deck:
+            self.round()
+
+        if not self.joueur.deck:
+            print(f"\n{self.joueur.nom} a perdu le combat !")
+        else:
+            print(f"\n{self.joueur_ennemi.nom} a perdu le combat !")
